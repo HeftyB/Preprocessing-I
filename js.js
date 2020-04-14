7f9122b32c9c96bd805bca9e3f6a6ae2e35aada6
Andrew Shields <andrewshields@Andrews-iMac.attlocal.net>
2020-04-13 22:53:14 -0400

    made js animation work with css animation

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 alert("HEY THIS IS SOME JAVASCRIPT");
 
-var textWrapper = document.querySelector('.TIT .letters');
+/*var textWrapper = document.querySelector('.TIT .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
 anime.timeline({loop: true})
@@ -12,6 +12,24 @@ anime.timeline({loop: true})
     delay: (el, i) => 45 * (i+1)
   }).add({
     targets: '.TIT',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });*/
+  // Wrap every letter in a span
+let textWrapper = document.querySelector('.mane .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.mane .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.mane',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
